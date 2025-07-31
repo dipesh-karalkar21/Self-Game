8e2c45d1e8b14b67ee04bb04ebc930cd99ebc95c
dipesh-karalkar21 <82707684+dipesh-karalkar21@users.noreply.github.com>
2025-07-31 09:41:22 +0530

    Update shoot.js

--- shoot.js
+++ shoot.js
@@ -10,12 +10,14 @@ AFRAME.registerComponent("shoot",{
 
                 console.log("bullet created")
 
-                bullet.setAttribute("gltf-model",`#bullet`)
                 bullet.setAttribute("rotation",{
                     x:0,
                     y:0,
                     z:0
                 })
+                
+                bullet.setAttribute("gltf-model",`#bullet`)
+                
 
                 bullet.setAttribute("scale",{
                     x:0.05,
